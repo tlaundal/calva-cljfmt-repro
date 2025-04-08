06d4ed86304473fe6081e6e49b4b6c83ad8fbbcb
Tobias Laundal <tobias.laundal@ardoq.com>
2025-04-08 16:55:00 +0200

    Add comments

--- repro/repro.clj
+++ repro/repro.clj
@@ -2,5 +2,6 @@
   (:require
    [repro.macro :as macro]))
 
+;; Calva and cljfmt fight for how this should be formatted
 (macro/example-macro :first-arg second-arg
-                     (some long body))
+  (some long body))
